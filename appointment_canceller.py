a1f133f54654b5732e162dfa8f6491f9e13b4a25
c-rotte <timmeyer12345678@gmail.com>
2024-09-05 07:25:28 +0200

    adjusted for multi-page appointments

--- appointment_canceller.py
+++ appointment_canceller.py
@@ -30,31 +30,46 @@ class AppointmentCanceller:
 
     def get_current_appointments(self):
         self._initialize_session()
-        app_l_info = ("QUFGQzR5TVpRWHpaY3Nua0tSVGx1VkFBWUQ0RytjWDhXS3hjcWpIZWl2akxOVXF0RFpZR3d0NFBYQ3"
-                      "VDRVhjWXpwOVlYUms5YzlFR3g5aWZIYkZPYzRUK0NuWHVucUFaeDhudVNzVEtQNUhtQXc9PQ==")
-        res = self.session.post("https://www.hikorea.go.kr/mypage/MypgNonResvPageR.pt", data={
-            "applInfo": app_l_info,
-            "authType": 3,
-            "pageGubun": "NonSearch",
-            "langCd": "en",
-            "pageUserId": "hikorea_3",
-            "TRAN_TYPE": "ComSubmit"
-        })
 
+        def get_res(page):
+            app_l_info = ("QUFGQzR5TVpRWHpaY3Nua0tSVGx1VkFBWUQ0RytjWDhXS3hjcWpIZWl2akxOVXF0RFpZR3d0NFBYQ3"
+                          "VDRVhjWXpwOVlYUms5YzlFR3g5aWZIYkZPYzRUK0NuWHVucUFaeDhudVNzVEtQNUhtQXc9PQ==")
+            res = self.session.post("https://www.hikorea.go.kr/mypage/MypgNonResvPageR.pt", data={
+                "page": page,
+                "applInfo": app_l_info,
+                "authType": 3,
+                "pageGubun": "NonSearch",
+                "langCd": "en",
+                "pageUserId": "hikorea_3",
+                "TRAN_TYPE": "ComSubmit"
+            })
+            return res
+
+        res = get_res(1)
         soup = BeautifulSoup(res.text, "html.parser")
-        appointments = []
-        for tr in soup.find_all("tr"):
-            tds = tr.find_all("td", class_="tac")
-            if len(tds) != 5:
-                continue
-            on_click = tds[0].find("a")["onclick"]
-            reservation_id = on_click.split("fncDetailResv('")[1].split("'")[0]
-            state = tds[3].text
-            if state != "Reserved":
-                continue
-            date = "".join(tds[1].text.strip().replace(".", "").split(" ")[0:3])
-            appointments.append({"id": reservation_id, "date": date})
-        return appointments
+        num_entries = int(soup.find("div", class_="total_count").find("span", class_="number").text)
+        num_pages = num_entries // 10 + 1
+
+        def get_appointments(soup):
+            for tr in soup.find_all("tr"):
+                tds = tr.find_all("td", class_="tac")
+                if len(tds) != 5:
+                    continue
+                on_click = tds[0].find("a")["onclick"]
+                reservation_id = on_click.split("fncDetailResv('")[1].split("'")[0]
+                state = tds[3].text
+                if state != "Reserved":
+                    continue
+                date = "".join(tds[1].text.strip().replace(".", "").split(" ")[0:3])
+                yield {"id": reservation_id, "date": date}
+
+        reserved = []
+        for page in range(0, num_pages):
+            reserved.extend(get_appointments(soup))
+            if page + 1 < num_pages:
+                res = get_res(page + 2)
+                soup = BeautifulSoup(res.text, "html.parser")
+        return reserved
 
     def cancel_appointment(self, reservation_id):
         self.session.post("https://www.hikorea.go.kr/mypage/MypgNonResvDetailR.pt", data={
